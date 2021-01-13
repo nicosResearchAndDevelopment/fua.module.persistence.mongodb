f3931c76c97d723721bd2b66421a8f8e93001bf9
Simon Petrac <pfoerdie@gmail.com>
2021-01-13 13:44:22 +0100

    preparations for final persistence interface

---
+++ src/module.persistence.mongodb.js
@@ -0,0 +1,5 @@
+const
+	DataFactory = require('../../module.persistence/src/module.persistence.js'),
+	DatasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js');
+
+// TODO
